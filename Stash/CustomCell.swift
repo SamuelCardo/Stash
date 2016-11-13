88be18a3456c651519d3942c1a62bd8af8920376
SamuelCardo <samuelcardo@SamuelCardos-Mac.local>
2016-11-13 20:05:02 +0100

    no message

---
+++ Stash/CustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCell.swift
+//  Stash
+//
+//  Created by SamuelCardo on 12/11/16.
+//  Copyright © 2016 CUBiC digital. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+}
--- Stash/ViewController.swift
+++ Stash/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 5;
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:indexPath) as! CustomCell
+        return cell
+    }
 }
 
